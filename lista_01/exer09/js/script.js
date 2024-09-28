b94b8e687ce796bf757b56e995abdbb050dd4468
clauvessouza <claudio.alves@estudante.iftm.com.br>
2024-09-28 18:59:09 -0300

    estudo

--- lista_01/exer09/js/script.js
+++ lista_01/exer09/js/script.js
@@ -2,7 +2,11 @@ expressao = prompt("Por favor, insira uma expressão (ex: ((5*8)-9) ou qualquer 
 if(expressao){
     try {
         resultado = eval(expressao);
-alert ('O resultado da expresao')
+alert ('O resultado da expresao' + expressao + ' é: ' + resultado);
 
     }
+    catch (erro){
+        alert("Expressão inválida! Por favor , insira uma expressão válida")
+    }
+
 }
